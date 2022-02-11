6d9fe84e575ad223bdb20e5af35168f024aa9738
Matt Powell <mpowell171@gmail.com>
2022-02-11 18:03:48 -0500

    Added image for nav bar home

--- project-files/src/App.js
+++ project-files/src/App.js
@@ -1,11 +1,19 @@
-import { useEffect } from 'react';
 import './App.css';
+import {Link, Route, Routes} from 'react-router-dom'
+
 
 function App() {
+
 
   
   return (
     <div className="App">
+
+      <nav>
+        <ul>
+          <Link to='/'><img src='./images/nav-icon.png' /></Link>
+        </ul>
+      </nav>
    
     </div>
   );
