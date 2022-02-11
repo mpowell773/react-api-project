d750f4406e297b84c4cb75cb362bb8e56efeb404
Matt Powell <mpowell171@gmail.com>
2022-02-11 12:48:25 -0500

    Created react app template and added project worksheet

---
+++ project-files/src/App.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  useEffect (() => {}, [])
+
+
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
