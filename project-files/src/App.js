a6b5ec1afe577b2e811d3b9aa8d0a4ad70563ea4
Matt Powell <mpowell171@gmail.com>
2022-02-11 18:20:15 -0500

    added routing functionality and have pages rendering properly, have bug with image for nav bar

--- project-files/src/App.js
+++ project-files/src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import {Link, Route, Routes} from 'react-router-dom'
+import LandingPage from './Components/LandingPage/LandingPage';
+import RandomPage from './Components/RandomPage/RandomPage';
+import SearchPage from './Components/SearchPage/SearchPage';
+import Bio from './Components/Bio/Bio';
 
 
 function App() {
@@ -10,10 +14,25 @@ function App() {
     <div className="App">
 
       <nav>
+      <Link to='/'><img src='./images/nav-icon.jpg' alt='a stick figure' /> </Link>
         <ul>
-          <Link to='/'><img src='./images/nav-icon.png' /></Link>
+         <Link to='/random'><li>Random Comic!</li></Link>
+         <Link to='/search'><li>Search for Comic!</li></Link>
+         <Link to='/bio'><li>Bio</li></Link>
+
         </ul>
       </nav>
+
+      <main>
+        <Routes>
+          <Route path='/' element={<LandingPage />}></Route>
+          <Route path='/random' element={<RandomPage/>}></Route>
+          <Route path='/search' element={<SearchPage />}></Route>
+          <Route path='/bio' element={<Bio />}></Route>
+
+        </Routes>
+      </main>
+     
    
     </div>
   );
