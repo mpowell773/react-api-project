40a4d6a37acd1eb29e00cf90cd377729a309933b
Matt Powell <mpowell171@gmail.com>
2022-02-14 08:24:12 -0500

    defined properties from Random to Comic

--- project-files/src/Components/RandomPage/RandomPage.js
+++ project-files/src/Components/RandomPage/RandomPage.js
@@ -1,28 +1,44 @@
-import './RandomPage.css'
-import {useEffect, useState} from 'react'
+import "./RandomPage.css";
+import { useEffect, useState } from "react";
+import Comic from "../Comic/Comic";
 
 const RandomPage = () => {
-    const [comic, setComic] = useState({});
+  const [comic, setComic] = useState({});
 
-    useEffect (() => {
-      //getRandomComic()
-    },[])
+  useEffect(() => {
+    //getRandomComic()
+  }, []);
+
+  //API Request
+  const getRandomComic = () => {
+    const url = "https://xkcd.now.sh?comic=latest";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setComic(data))
+      .catch(() => console.log("no comics for you Sadge"));
+  };
 
 
-    //API Request
-    const getRandomComic = () => {
-        const url = 'https://xkcd.now.sh?comic=latest'
-
-        fetch(url)
-            .then ((response) => response.json())
-            .then ((data) => setComic(data))
-            .catch (() => console.log('no comics for you Sadge'))
-    }
-
-    return (
-        <h3>Random</h3>
-    )
-
-} 
+// Rendering our page and passing properties into Comic
+  return (
+    <div>
+      <h3>Random</h3>
+      <Comic
+        alt={comic.alt}
+        day={comic.day}
+        img={comic.img}
+        link={comic.link}
+        month={comic.month}
+        news={comic.news}
+        num={comic.num}
+        safe_title={comic.safe_title}
+        title={comic.title}
+        transcript={comic.transcript}
+        year={comic.year}
+      />
+    </div>
+  );
+};
 
 export default RandomPage;
